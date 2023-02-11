ebae8dae491620b94b7d001be75589652a1a4a2e
kaybrian <kayonbrian@gmail.com>
2023-02-11 19:16:39 +0200

    starting the designs

--- cilent/src/containers/LoginPage.js
+++ cilent/src/containers/LoginPage.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from 'react-redux';
+
 
 
 const Login = () => {
+    const [formdata, setFormData] = useState({
+        email: "",
+        password: ""
+    });
+
+    const { email, password } = formdata;
+
+    const onchange = e => {
+        setFormData({
+            ...formdata,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const onSubmit = e =>{
+        e.preventDefault();
+
+        // login functions
+
+    }
+
+    // is the user Authenticated :
+    // redirect to the home page
+
     return (
-        <div>
-            <h1>Login</h1>
+        <div  className="container mt-5">
+            <h1>Sign In</h1>
+            <p>Sign into your account</p>
+
+            <form onSubmit={e => onSubmit(e)}>
+
+            </form>
+
         </div>
     )
 };
