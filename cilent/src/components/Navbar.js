ba6d6929675d55a5ba2f9847a57ef1559df24de6
kaybrian <kayonbrian@gmail.com>
2023-02-13 14:02:09 +0200

    works for the login and out

--- cilent/src/components/Navbar.js
+++ cilent/src/components/Navbar.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import { logout } from "../actions/auth";
 import { connect } from "react-redux";
 
 
-const Navbar = () => {
-    const gustLinks = () => {
+const Navbar = ({ logout, isAuthenticated }) => {
 
-    }
-    const authLinks = () => {
+    const gustLinks = () => (
+        <Fragment>
+            <li className="nav-item">
+                <Link className="nav-link" to="/login">Login</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/signup">Signup</Link>
+            </li>
+        </Fragment>
+    )
 
+    const authLinks = () => (
+        <li className="nav-item">
+            <a className="nav-link" href="#!" onClick={logoutHandler}>Logout</a>
+        </li>
+    )
+
+    const logoutHandler = () => {
+        logout();
     }
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Auth system</a>
+                <Link className="navbar-brand" to="#">Auth system</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -23,15 +38,7 @@ const Navbar = () => {
                         <li className="nav-item">
                             <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                         </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/login">Login</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/signup">Signup</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/signup">Logout</Link>
-                        </li>
+                        {isAuthenticated ? authLinks(): gustLinks()}
                     </ul>
                 </div>
             </div>
@@ -39,5 +46,9 @@ const Navbar = () => {
     )
 };
 
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+})
 
-export default connect(null, {logout})(Navbar);
+
+export default connect(mapStateToProps, { logout })(Navbar);
